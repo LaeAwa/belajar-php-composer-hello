fef7b5f4e86850640e71fbf770f46af765f8022c
LaeAwa <fatwaabdullah48@gmail.com>
2022-10-02 01:52:53 +0700

    contoh library sederhana composer

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => 'laeawa/bephp-composer-hello',
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
-        'reference' => NULL,
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '3d40d88057607447ea96b6d7e7607b55cd42bfff',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         'laeawa/bephp-composer-hello' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
-            'reference' => NULL,
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '3d40d88057607447ea96b6d7e7607b55cd42bfff',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
